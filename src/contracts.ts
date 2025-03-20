ebf41ac7bb3f81de4e9d8f17008d27453f1ab8cd
Shamsundar Dileep <dileepfrog@gmail.com>
2025-03-20 10:56:21 -0700

    feat: add new typechain types

--- src/contracts.ts
+++ src/contracts.ts
@@ -5,21 +5,18 @@ import * as factories from '#typechain-types/factories/index';
  *
  * @category Smart Contracts
  */
-export const EarningsEscrowContract =
-  factories.contracts.EarningsEscrow__factory;
+export const EarningsEscrowContract = factories.EarningsEscrow__factory;
 
 /**
  * A typechain factory for interacting with Kuma's Exchange v1 contract.
  *
  * @category Smart Contracts
  */
-export const Exchangev1Contract =
-  factories.contracts.exchangeSol.Exchange_v1__factory;
+export const Exchangev1Contract = factories.Exchange_v1__factory;
 
 /**
  * A typechain factory for interacting with ERC20 contracts.
  *
  * @category Smart Contracts
  */
-export const ERC20Contract =
-  factories.openzeppelin.contracts.token.erc20.ERC20__factory;
+export const ERC20Contract = factories.ERC20__factory;
